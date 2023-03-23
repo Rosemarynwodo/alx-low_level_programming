35019f37cf64b31b2819a1d29fa99ab60199a3ca
Rosemarynwodo <rn.nwodo@gmail.com>
2023-03-23 16:07:36 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,14 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 if in other case.
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	char c;
-	if c = 'A';
+	if (c >= 65 && c <= 90)
 	{
-	printf("%c: %d\n", c, _isupper(c));
-	return (1);
+		return (1);
 	}
 	else
 	{
-		c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+		return (0);
 	}
 }
