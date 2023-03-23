0fa5af18f54ebf2da91b6489ca6fc7ca29a9f8f3
Rosemarynwodo <rn.nwodo@gmail.com>
2023-03-23 15:06:02 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * Return: 1 if is an uppercase character, 0 if in other case.
+ */
+int main(void)
+{
+	char c;
+	if c = 'A';
+	{
+		printf("%c: %d\n", c, _isupper(c));
+		return (1);
+	}
+	else
+	{
+		c = 'a';
+		printf("%c: %d\n", c, _isupper(c));
+		return (0);
+	}
+}
