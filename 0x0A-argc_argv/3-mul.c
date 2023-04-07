39e2b462ecfd4f4c1b2d388cb96ad1bcc9995891
Rosemarynwodo <rn.nwodo@gmail.com>
2023-04-07 10:19:29 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int b, c, diff;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	b = atoi(argv[1]);
-	c = atoi(argv[2]);
-	diff = b * c;
-
-	printf("%d\n", diff);
+	
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	
 	return (0);
+
 }
