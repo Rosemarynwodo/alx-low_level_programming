a87ea9286f85987812d86f385a6fd1260e0a181a
Rosemarynwodo <rn.nwodo@gmail.com>
2023-07-08 04:52:01 -0700

    updated 0-binary file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len;
+	unsigned int value = 0;
 
 	if (b == NULL)
 		return (0);
-
-	for (len = 0; b[len]; len++)
+	while (*b != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
+		value = value << 1;
+		if (*b != '1' && *b != '0')
 			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
 	}
-
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (value);
 }
 
